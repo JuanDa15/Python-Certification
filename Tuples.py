75445dbfd6c4be1b5ddd9ad98fbab7146d19d37d
Juan David Osorio Ortiz <jdoo1115@gmil.com>
2020-07-04 12:00:00 -0500

    first commit

---
+++ Tuples.py
@@ -0,0 +1,26 @@
+#ORDER WORDS WITH TUPLES
+Filename = input("Enter File name: ")
+try:
+    Document = open(Filename,"r")
+except:
+    print("Document" + Filename + "Doesnt exist")
+
+HoursList = list()
+
+for line in Document:
+    if line.startswith("From "):
+        SplitedLine = line.split()
+        RecortedHour = SplitedLine[5]
+        HoursList.append(RecortedHour.split(":")[0])
+
+Dictionary = dict()
+for Hour in HoursList:
+    Dictionary[Hour] = Dictionary.get(Hour,0) + 1
+
+HoursList2 = list()
+for key,value in Dictionary.items():
+    NewItem = (key,value)
+    HoursList2.append(NewItem)
+    
+for key,value in sorted(HoursList2):
+    print(key,value)
