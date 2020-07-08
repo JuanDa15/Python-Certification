597d04a79ea6c250e586a416d6ed0aff3b231157
JuanDa15 <59321452+JuanDa15@users.noreply.github.com>
2020-07-08 10:37:13 -0500

    g

--- networks.py
+++ networks.py
@@ -2,13 +2,13 @@ import socket as s
 from socket import *
 
 mysocket = s.socket(s.AF_INET,s.SOCK_STREAM)
-mysocket.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysocket.connect(('104.31.80.213', 443))
+cmd = 'GET https://www.tempoeficaz.com HTTP/1.0\r\n\r\n'.encode()
 mysocket.send(cmd)
 
 
 while(True):
-    data = mysocket.recv(512)
+    data = mysocket.recv(1024)
     if (len(data)  < 1): break
     print(data.decode()) 
 mysocket.close()
